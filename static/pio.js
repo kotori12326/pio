9093f8afdc3d402b2b10a80c82b8844ad8e4a9a7
kotori12326 <162541901+kotori12326@users.noreply.github.com>
2025-08-22 21:29:21 +0800

    Add files via upload

--- static/pio.js
+++ static/pio.js
@@ -173,7 +173,7 @@ var Paul_Pio = function (prop) {
                 // 设置默认显示位置，靠右下但不要太低
                 current.body.style.top = "auto";
                 current.body.style.right = "20px";
-                current.body.style.bottom = "100px";
+                current.body.style.bottom = "200px";
             }
 
             switch (prop.mode) {
@@ -212,7 +212,7 @@ var Paul_Pio = function (prop) {
 
             // 设置显示位置
             current.body.style.right = "20px";
-            current.body.style.bottom = "100px";
+            current.body.style.bottom = "200px";
 
             // 触发其他初始化操作（按钮、拖动等）
             switch (prop.mode) {
